29ef3f9150b5cd10d169a38441d7f2343d3e5260
Stefan Kimmer <skimmer@s2e-systems.com>
2023-04-06 14:05:11 +0200

    Add files from dust-dds

---
+++ tests/examples/enums.rs
@@ -0,0 +1,14 @@
+#[derive(Debug, serde::Deserialize, serde::Serialize, dust_dds::topic_definition::type_support::DdsSerde, dust_dds::topic_definition::type_support::DdsType)]
+pub enum Suits {
+    Spades,
+    Hearts,
+    Diamonds,
+    Clubs,
+}
+#[derive(Debug, serde::Deserialize, serde::Serialize, dust_dds::topic_definition::type_support::DdsSerde, dust_dds::topic_definition::type_support::DdsType)]
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
